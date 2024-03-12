8686291bbc163ef2c0b91216ce28bfa10b8c39e0
MLAIKAH <mlaikahimdad@gmail.com>
2024-03-12 10:52:42 +0500

    Removed Credentials line

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
   headers: { Authorization: `Bearer ${sessionStorage.getItem("accessToken")}` },
-  withCredentials: true,
 });
 
 
